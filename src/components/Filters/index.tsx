369f41bb6ee3eed03395e671a046e166f39fe1f9
VarTy <talankina.v@mail.ru>
2023-10-15 02:43:21 +0300

    vt search fix

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -6,15 +6,15 @@ export default function Filter() {
         <div className='filters'>
             <div className='switch'>
                 <div>
-                    <input type="checkbox" id="atm" name='atm' />
+                    <input className="input-filter"type="checkbox" id="atm" name='atm' />
                     <label htmlFor="atm"></label>
                 </div>
                 <p>Показать банкоматы</p>
             </div>
             <div className='switch'>
                 <div>
-                    <input type="checkbox" id="office" name='office' />
-                    <label htmlFor="office"></label>
+                    <input className="input-filter"type="checkbox" id="office" name='office' />
+                    <label className='label-filter' htmlFor="office"></label>
                 </div>
                 <p>Показать офисы</p>
             </div>
